d02a9b63028a60b9d18dc00d688f670e4fb1f3d1
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-23 14:11:23 -0400

    No significant commits
    have to go back to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,44 @@ var searchBtn = $("#searchBtn");
 var cityBox = $("#cityBox");
 var cityList = JSON.parse(localStorage.getItem("city")) || [];
 
+function currentWeather (city){
+    var serverSide = apiLink = "?q=" + city + "&appid=" + apiKey + fahrenheit;
+    fetch(serverSide).then(function(response) {
+        if(response.ok){
+            response.json().then (function(weatherData){
+                getUvIndex(city, weatherData);
+                getForecast(weatherData);
+                saveSearch(city);
+                displayCities();
+            });
+        }
+    });  
+}
+
+function getUVIndex(city,weatherData) {
+    var uvAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon=" + weatherData.coord.lon + "&appid=" + apiKey;
+    fetch(uvAPI).then(function(response){
+        if(response.ok){
+            response.json().then(function(uvData){
+                displayWeather(city, weatherData, uvData);
+            });
+        }
+    });
+}
+
+function displayWeather (city, weatherData, uvData){
+
+
+    //display current city/date
+    var cityHeader = $("<h2>")
+        .addClas("capital")
+        .text(city + "(" + month + "/" + day + "/" + year + ") ");
+    var imgEl = $("<img>").attr("src","http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + ".png" );
+    cityHeader.append(imgEl);
+    currentWeatherBox.append(cityHeader);
+
+    
+}
 
 
 
