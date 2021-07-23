40c67f4cbdd7f4fd0160ba99da8e7265c4a0c782
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-23 12:59:11 -0400

    started work on JS
    added global variables

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//Global Variables
+var apiLink = "https://api.openweathermap.org/data/2.5/forecast";
+var apiKey = "df21af596a60ec7e877a04968712a9d3";
+var tempUnit = "&units=imperial";
+var currentDate = new Date();
+var month = currentDate.getDate()+1;
+var day = currentDate.getDay();
+var year = currentDate.getFullYear();
+var currentWeatherBox = ("#currentWeather")
+//Button Click
+
+
+
